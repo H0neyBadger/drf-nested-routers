8763fc0b610bc051c258ee0f591fb69637183221
H0neyBadger <H0neyBadger@users.noreply.github.com>
2015-02-10 11:04:35 +0100

    Add support for lookup_value_regex and DefaultRouter format suffixes

--- rest_framework_nested/routers.py
+++ rest_framework_nested/routers.py
@@ -30,21 +30,7 @@ from __future__ import unicode_literals
 import rest_framework.routers
 
 
-class SimpleRouter(rest_framework.routers.SimpleRouter):
-    """ Improvement of rest_framework.routers.SimpleRouter that allows the
-    lookup of urls of nested resources.
-
-    """
-    def get_lookup_regex(self, viewset, lookup_prefix=''):
-        """
-        Given a viewset, return the portion of URL regex that is used
-        to match against a single instance.
-        """
-        base_regex = '(?P<{lookup_prefix}{lookup_field}>[^/]+)'
-        lookup_field = getattr(viewset, 'lookup_field', 'pk')
-        return base_regex.format(lookup_field=lookup_field, lookup_prefix=lookup_prefix)
-
-class NestedSimpleRouter(SimpleRouter):
+class NestedSimpleRouter(rest_framework.routers.DefaultRouter):
     def __init__(self, parent_router, parent_prefix, *args, **kwargs):
         self.parent_router = parent_router
         self.parent_prefix = parent_prefix
